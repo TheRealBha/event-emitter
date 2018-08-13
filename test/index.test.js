46e90da8a9ddb1e28805a75b0871207bf8643a11
Brian Ha <brianhainbox@gmail.com>
2018-08-13 14:32:13 -0700

    Add tests.

--- test/index.test.js
+++ test/index.test.js
@@ -1,2 +1,52 @@
 'use strict';
-const eventEmitter = require('../lib/index');
+
+const EventEmitter = require('../lib/index');
+
+const EE = new EventEmitter();
+
+describe('EventEmitter', () => {
+    const mockCallBack = jest.fn();
+    const mockCallBack2 = jest.fn();
+
+    beforeEach(() => {
+        mockCallBack.mockClear();
+        mockCallBack2.mockClear();
+    })
+        
+    test('It should register handler functions for named events', () => {
+        EE.on('testEvent', mockCallBack);
+        EE.on('testEvent', mockCallBack2);
+        expect(EE.events['testEvent'].length).toBe(2);
+    });
+
+    test('It should register "one-time" handler that will be called at most one time.', () => {
+
+        EE.on('testEvent', mockCallBack2)
+        EE.once('testEvent', mockCallBack);
+        expect(EE.events['testEvent'].length).toBe(4);
+        EE.emit('testEvent', 'mockArg');
+        expect(mockCallBack).toHaveBeenCalledTimes(2);
+        expect(EE.events['testEvent'].length).toBe(3);
+    })
+
+    it('should emit named events with any number of arguments.', () => {
+        const testArguments = jest.fn().mockImplementation((arg1, arg2, arg3, arg4, arg5) => {}); 
+        EE.on('testEvent', testArguments);
+        EE.emit('testEvent', 'arg1', 'arg2', 'arg3', 'arg4', 'arg5');
+        expect(testArguments).toHaveBeenCalledWith('arg1', 'arg2', 'arg3', 'arg4', 'arg5');
+    });
+
+    test('It should remove specific previously-registered event handlers', () => {
+        const mockCallBack3 = () => console.log('');
+        EE.on('testEvent', mockCallBack3)
+        expect(EE.events['testEvent'].length).toBe(5);
+        EE.clear('testEvent', mockCallBack3);
+        expect(EE.events['testEvent'].length).toBe(4);
+
+    });
+
+    test('It should remove all previously-registered event handlers', () => {
+        EE.clearAll('testEvent');
+        expect(EE.events['testEvent']).toBe(undefined);
+    });
+});
